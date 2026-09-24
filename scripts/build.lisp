1a79f00b16dfe0419e7aa9caa9981ead20d39dc0
Cian Hughes <chughes000@gmail.com>
2026-09-24 15:13:10 +0000

    build: do not auto-increment CalVer version on build

--- scripts/build.lisp
+++ scripts/build.lisp
@@ -6,13 +6,6 @@
                        (uiop:pathname-parent-directory-pathname
                          (uiop:pathname-directory-pathname this-file))
                        (uiop:getcwd))))
-  ;; Automatically synchronize version.txt before building
-  (let ((calver-script (merge-pathnames "scripts/calver.lisp" project-root)))
-    (when (probe-file calver-script)
-      (ignore-errors
-        (uiop:run-program (list (namestring calver-script) "--update")
-                          :output *standard-output*
-                          :error-output *error-output*))))
   (pushnew project-root asdf:*central-registry* :test #'equal)
   (asdf:load-asd (merge-pathnames "structural-editing-mcp.asd" project-root)))
 
